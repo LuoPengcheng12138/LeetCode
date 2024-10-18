004b1158584f97116b4b8329c9a376bcdd28bbe5
LuoPengcheng <2653972504@qq.com>
2024-10-18 19:26:18 +0800

    LeetCode 611

---
+++ src/LeetCode611.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.HashMap;
+class LeetCode611 {
+    public int triangleNumber(int[] nums) {
+        int len=nums.length;
+        Arrays.sort(nums);
+        int count=0;
+        for(int k=2;k<len;k++){
+            int i=0;
+            int j=k-1;
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    //意味着
+                    //nums[i+1]+nums[j]必大于nums[k]
+                    //···
+                    //nums[i+*]+nums[j]必大于nums[k]
+                    count+=(j-i);
+                    j--;
+                }else{
+                    //意味着
+                    //nums[i]+nums[j-1]必大于nums[k]
+                    //···
+                    //nums[i]+nums[j-*]必大于nums[k]
+                    //i不可能是形成最短边
+                    i++;
+                }
+            }
+        }
+        return count;
+       
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] input={2,3,4,4};
+        LeetCode611 leetcode=new LeetCode611();
+        leetcode.triangleNumber(input);
+        
+        
+    }
+}
